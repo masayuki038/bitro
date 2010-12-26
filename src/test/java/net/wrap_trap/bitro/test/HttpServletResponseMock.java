22cb7f4f9022abca7c345da0271a82ecf0339e0c
masayuki038 <masayuki038@gmail.com>
2010-12-27 00:55:27 +0900

    adjust the folders of source code files.

--- src/test/java/net/wrap_trap/bitro/test/HttpServletResponseMock.java
+++ src/test/java/net/wrap_trap/bitro/test/HttpServletResponseMock.java
@@ -202,28 +202,4 @@ public class HttpServletResponseMock implements HttpServletResponse{
 		// TODO Auto-generated method stub
 		
 	}
-
-	@Override
-	public String getHeader(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Collection<String> getHeaderNames() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Collection<String> getHeaders(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getStatus() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 }
